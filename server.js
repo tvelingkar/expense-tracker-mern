b3da395aab43556aa9c3b9b91d811444bd726d5c
Tushar Velingkar <Tushar.Velingkar@ibm.com>
2023-01-23 11:33:45 +0530

    Updated environments

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
-const colors = require('colors');
+require('colors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
+const cors = require('cors')
 
 dotenv.config({ path: './config/config.env' });
 
@@ -14,6 +15,7 @@ const transactions = require('./routes/transactions');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 if(process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
